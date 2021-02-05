3d108851df6f8f5b8de7e504e99bf7e3f9e15410
ajizul22 <70998823+ajizul22@users.noreply.github.com>
2021-02-05 16:16:51 +0700

    fix authorization search

--- src/routers/engineer.js
+++ src/routers/engineer.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 
 const { getAllEng, getEngById, updateEng, getEngIdByAcId, getDetailEng } = require('../controllers/engineer')
-const { authorizationEngineer, authorizationCompany } = require('../middleware/auth')
+const { authorizationEngineer } = require('../middleware/auth')
 
 const router = Router()
 
 const uploadImage = require('../middleware/multer')
 
-router.get('/', authorizationCompany, getAllEng)
+router.get('/', getAllEng)
 router.get('/detail', getDetailEng)
 router.get('/:enId', getEngById)
 router.put('/:enId', authorizationEngineer, uploadImage, updateEng)
